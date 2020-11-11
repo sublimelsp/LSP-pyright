9a6597b92febc72fe46fd6c2cace27311c4a332b
Jack Cherng <jfcherng@gmail.com>
2020-11-11 17:06:28 +0800

    feat: add a setting to add ST package deps into extraPath (#26)

--- plugin.py
+++ plugin.py
@@ -1,7 +1,8 @@
 import os
 import sublime
+import sys
 
-from LSP.plugin.core.typing import Dict, Optional, Tuple
+from LSP.plugin.core.typing import Any, Dict, List, Tuple
 from lsp_utils import NpmClientHandler
 
 
@@ -19,20 +20,37 @@ class LspPyrightPlugin(NpmClientHandler):
     server_binary_path = os.path.join(server_directory, "node_modules", "pyright", "langserver.index.js")
 
     @classmethod
-    def additional_variables(cls) -> Optional[Dict[str, str]]:
-        variables = super().additional_variables() or {}
-        variables.update(
-            {
-                "sublime_py_files_dir": os.path.dirname(sublime.__file__),
-            }
-        )
-
-        return variables
-
-    @classmethod
     def install_in_cache(cls) -> bool:
         return False
 
     @classmethod
     def minimum_node_version(cls) -> Tuple[int, int, int]:
         return (12, 0, 0)
+
+    @classmethod
+    def on_settings_read(cls, settings: sublime.Settings) -> bool:
+        super().on_settings_read(settings)
+
+        if settings.get("dev_environment") == "sublime_text":
+            server_settings = settings.get("settings", {})  # type: Dict[str, Any]
+
+            # add package dependencies into "python.analysis.extraPaths"
+            extraPaths = server_settings.get("python.analysis.extraPaths", [])  # type: List[str]
+            extraPaths.extend(cls.find_package_dependency_dirs())
+            server_settings["python.analysis.extraPaths"] = extraPaths
+
+            settings.set("settings", server_settings)
+
+        return False
+
+    @staticmethod
+    def find_package_dependency_dirs() -> List[str]:
+        dep_dirs = sys.path.copy()
+
+        # move the "Packages/" to the last
+        # @see https://github.com/sublimelsp/LSP-pyright/pull/26#discussion_r520747708
+        packages_path = sublime.packages_path()
+        dep_dirs.remove(packages_path)
+        dep_dirs.append(packages_path)
+
+        return [path for path in dep_dirs if os.path.isdir(path)]
