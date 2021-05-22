a4d323941591c705a167b1d10aabcd2192ab6bfe
Jack Cherng <jfcherng@gmail.com>
2021-05-22 10:46:22 +0800

    feat: add "dev_environment" for "sublime_text_38" (#66)

--- plugin.py
+++ plugin.py
@@ -1,7 +1,8 @@
 from LSP.plugin import DottedDict
-from LSP.plugin.core.typing import Any, List, Optional, Tuple
+from LSP.plugin.core.typing import Any, List, Optional, Tuple, cast
 from lsp_utils import NpmClientHandler
 import os
+import re
 import sublime
 import sys
 
@@ -26,10 +27,18 @@ class LspPyrightPlugin(NpmClientHandler):
     def on_settings_changed(self, settings: DottedDict) -> None:
         super().on_settings_changed(settings)
 
-        if self.get_plugin_setting("dev_environment") == "sublime_text":
+        dev_environment = self.get_plugin_setting("dev_environment")
+
+        if dev_environment in ("sublime_text", "sublime_text_33", "sublime_text_38"):
+            if dev_environment == "sublime_text":
+                # the Python version this plugin runs on
+                py_ver = cast(Tuple[int, int], tuple(sys.version_info[:2]))
+            else:
+                py_ver = (3, 8) if dev_environment == "sublime_text_38" else (3, 3)
+
             # add package dependencies into "python.analysis.extraPaths"
             extraPaths = settings.get("python.analysis.extraPaths") or []  # type: List[str]
-            extraPaths.extend(self.find_package_dependency_dirs())
+            extraPaths.extend(self.find_package_dependency_dirs(py_ver))
             settings.set("python.analysis.extraPaths", extraPaths)
 
     # -------------- #
@@ -41,8 +50,14 @@ class LspPyrightPlugin(NpmClientHandler):
         return sublime.load_settings(cls.package_name + ".sublime-settings").get(key, default)
 
     @staticmethod
-    def find_package_dependency_dirs() -> List[str]:
+    def find_package_dependency_dirs(py_ver: Tuple[int, int] = (3, 3)) -> List[str]:
         dep_dirs = sys.path.copy()
+
+        # replace paths for target Python version
+        # @see https://github.com/sublimelsp/LSP-pyright/issues/28
+        re_pattern = r"(python3\.?)[38]"
+        re_replacement = r"\g<1>8" if py_ver == (3, 8) else r"\g<1>3"
+        dep_dirs = [re.sub(re_pattern, re_replacement, d, flags=re.IGNORECASE) for d in dep_dirs]
 
         # move the "Packages/" to the last
         # @see https://github.com/sublimelsp/LSP-pyright/pull/26#discussion_r520747708
