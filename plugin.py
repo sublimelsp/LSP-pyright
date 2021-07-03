3b60277cdc4529c952d6674ffb16c469326bb572
Jack Cherng <jfcherng@gmail.com>
2021-07-04 01:37:08 +0800

    refactor: copy the whole resource/ directory
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -45,9 +45,9 @@ class LspPyrightPlugin(NpmClientHandler):
     @classmethod
     def install_or_update(cls) -> None:
         super().install_or_update()
-        # Copy typings
-        src = 'Packages/{}/resources/typings/'.format(cls.package_name)
-        dest = os.path.join(cls.package_storage(), 'typings')
+        # Copy resources
+        src = "Packages/{}/resources/".format(cls.package_name)
+        dest = os.path.join(cls.package_storage(), "resources")
         ResourcePath(src).copytree(dest, exist_ok=True)
 
     # -------------- #
@@ -83,6 +83,6 @@ class LspPyrightPlugin(NpmClientHandler):
         dep_dirs.append(packages_path)
 
         # sublime stubs - add as first
-        dep_dirs.insert(0, os.path.join(self.package_storage(), 'typings', 'sublime_text'))
+        dep_dirs.insert(0, os.path.join(self.package_storage(), "resources", "typings", "sublime_text"))
 
         return [path for path in dep_dirs if os.path.isdir(path)]
