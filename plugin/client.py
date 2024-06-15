210572c844c3b8134c0ce58548454dff876f4372
Jack Cherng <jfcherng@gmail.com>
2024-06-16 05:54:58 +0800

    refactor: better statusText template
    The "server_version" will not be available at the very beginning.
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/client.py
+++ plugin/client.py
@@ -7,9 +7,8 @@ import sys
 from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
-from typing import cast
+from typing import Any, cast
 
-import jinja2
 import sublime
 from LSP.plugin import ClientConfig, DottedDict, MarkdownLangMap, Response, WorkspaceFolder
 from LSP.plugin.core.protocol import CompletionItem, Hover, SignatureHelp
@@ -18,6 +17,7 @@ from sublime_lib import ResourcePath
 
 from .constants import PACKAGE_NAME
 from .log import log_info, log_warning
+from .template import render_template
 from .venv_finder import VenvInfo, find_venv_by_finder_names, get_finder_name_mapping
 
 
@@ -126,21 +126,25 @@ class LspPyrightPlugin(NpmClientHandler):
             return
         window_id = session.window.id()
 
-        variables = {
+        variables: dict[str, Any] = {
             "server_version": "",  # no way to get it?
             "venv": {},
         }
 
         if venv_info := self.window_attrs[window_id].venv_info:
-            variables["venv"].update(  # type: ignore
+            variables["venv"].update(
                 finder_name=venv_info.meta.finder_name,
                 python_version=venv_info.python_version,
                 venv_prompt=venv_info.prompt,
             )
 
-        template_text = str(session.config.settings.get("statusText") or "")
-        template_obj = jinja2.Environment().from_string(template_text)
-        session.set_config_status_async(template_obj.render(variables))
+        rendered_text = ""
+        if template_text := str(session.config.settings.get("statusText") or ""):
+            try:
+                rendered_text = render_template(template_text, variables)
+            except Exception as e:
+                log_warning(f'Invalid "statusText" template: {e}')
+        session.set_config_status_async(rendered_text)
 
     def patch_markdown_content(self, content: str) -> str:
         # add another linebreak before horizontal rule following fenced code block
