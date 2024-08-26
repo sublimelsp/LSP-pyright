b9ea8e90a5b175d22092601811d8b3b5a9d79646
Raoul Wols <raoulwols@gmail.com>
2024-08-26 16:28:49 +0200

    refactor: dev_environment handlers abstraction fixup

--- plugin/dev_environment/interfaces.py
+++ plugin/dev_environment/interfaces.py
@@ -7,6 +7,7 @@ from typing import Any, Iterable, Sequence, final
 from LSP.plugin.core.collections import DottedDict
 
 from ..constants import SERVER_SETTING_ANALYSIS_EXTRAPATHS, SERVER_SETTING_DEV_ENVIRONMENT
+from ..log import log_info
 from ..utils import camel_to_snake, remove_suffix
 
 
@@ -31,7 +32,7 @@ class BaseDevEnvironmentHandler(ABC):
     @classmethod
     def get_dev_environment_subsetting(cls, settings: DottedDict, subkey: str) -> Any:
         """Gets the sub-setting of `XXX.dev_environment.CLS_NAME.SUBKEY`."""
-        return settings.get(f"{SERVER_SETTING_DEV_ENVIRONMENT}.{cls.name()}.{subkey}")
+        return settings.get(f"{SERVER_SETTING_DEV_ENVIRONMENT}_{cls.name()}.{subkey}")
 
     @classmethod
     def can_support(cls, dev_environment: str) -> bool:
@@ -46,4 +47,5 @@ class BaseDevEnvironmentHandler(ABC):
         """Appends the given `paths` to `XXX.analysis.extraPaths` setting."""
         extra_paths: list[str] = settings.get(SERVER_SETTING_ANALYSIS_EXTRAPATHS) or []
         extra_paths.extend(map(str, paths))
+        log_info(f"Adding extra analysis paths: {paths}")
         settings.set(SERVER_SETTING_ANALYSIS_EXTRAPATHS, extra_paths)
